f0cd4683e5de5861e24435dbeb371bec98e461bf
zjyzju <zjyzju@163.com>
2017-01-17 18:45:54 +0800

    remove zPaas dir

--- zPaaS/src/main/java/com/zpaas/message/MessageSender.java
+++
@@ -1,8 +0,0 @@
-package com.zpaas.message;
-
-
-
-public interface MessageSender {
-	public void sendMessage(Object message, String topic);
-	public void sendMessage(Object message, String topic, String key);
-}
