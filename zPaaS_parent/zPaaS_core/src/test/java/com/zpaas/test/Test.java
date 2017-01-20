fecf56acf9909ce196665007099cbdaab67c4ab3
zjyzju <zjyzju@163.com>
2017-01-20 18:27:45 +0800

    add test code

---
+++ zPaaS_parent/zPaaS_core/src/test/java/com/zpaas/test/Test.java
@@ -0,0 +1,14 @@
+package com.zpaas.test;
+
+
+
+import com.zpaas.ConfigurationCenter;
+import com.zpaas.utils.PaasContextHolder;
+
+public class Test {
+	public static void main(String[] args)throws Exception {
+		//加密服务
+		ConfigurationCenter cc = (ConfigurationCenter) PaasContextHolder.getContext().getBean("cc");
+		System.out.println(cc.getRunMode());
+	}
+}
